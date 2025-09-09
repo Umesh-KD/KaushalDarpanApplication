6cfb207c9620af319ae8ab35827e5d6830ddb55d
Aadil Ali <aadil.ali@corpnet.co.in>
2025-09-09 16:33:15 +0530

    changes

--- Kaushal_Darpan_Application/src/app/Views/ITI/IIP-Module/iip-manage-admin-report/iip-manage-admin-report.component.ts
+++ Kaushal_Darpan_Application/src/app/Views/ITI/IIP-Module/iip-manage-admin-report/iip-manage-admin-report.component.ts
@@ -570,7 +570,7 @@ export class IIPManageAdminReportComponent {
 
             const link = document.createElement('a');
             link.href = blobUrl;
-            link.download = 'AllotmentCollege.pdf';
+            link.download = 'IIPMergedReport.pdf';
             document.body.appendChild(link);
             link.click();
             document.body.removeChild(link);
