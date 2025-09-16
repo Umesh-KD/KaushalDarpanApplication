9fd0c2b5e487b9ec0bb5a6e4aa0f6872fc9166de
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2025-09-16 17:51:53 +0530

    IIP module

--- Kaushal_Darpan_Application/src/app/app-routing.module.ts
+++ Kaushal_Darpan_Application/src/app/app-routing.module.ts
@@ -746,7 +746,7 @@ const routes: Routes = [
       { path: 'addstudent', loadChildren: () => import('./Views/Student/add-student/add-student.module').then(m => m.AddStudentModule) },
       { path: 'IndustryInstitutePartnershipList', loadChildren: () => import('./Views/IndustryInstitutePartnershipMaster/industry-institute-partnership-master/industry-institute-partnership-master.module').then(m => m.IndustryInstitutePartnershipMasterModule) },
       { path: 'Add-IndustryInstitutePartnership', loadChildren: () => import('./Views/IndustryInstitutePartnershipMaster/add-industry-institute-partnership-master/add-industry-institute-partnership-master.module').then(m => m.AddIndustryInstitutePartnershipMasterModule) },
-      { path: 'industryInstitutePartnership-validation', loadChildren: () => import('./Views/industry-institute-partnership-validation/industry-institute-partnership-validation.module').then(m => m.IndustryInstitutePartnershipValidationModule) },
+      { path: 'industryInstitutePartnership-validation', loadChildren: () => import('./Views/IndustryInstitutePartnershipMaster/industry-institute-partnership-validation/industry-institute-partnership-validation.module').then(m => m.IndustryInstitutePartnershipValidationModule) },
       { path: 'EditMeritDocument', loadChildren: () => import('./Views/BTER/BterMerit/edit-merit-document/edit-merit-document.module').then(m => m.EditMeritDocumentModule) },
       { path: 'AllotmentConfiguration', loadChildren: () => import('./Views/master-configuration/allotment-configuration/allotment-configuration.module').then(m => m.AllotmentConfigurationModule) },
 
