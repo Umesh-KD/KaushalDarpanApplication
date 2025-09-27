b534745aaa6963b89f30bcc5eb10d540a663bdc4
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2025-09-27 12:53:34 +0530

    counselling module

--- Kaushal_Darpan_Application/src/app/Services/CounsellingApplicationForm/counselling-application-form.service.ts
+++ Kaushal_Darpan_Application/src/app/Services/CounsellingApplicationForm/counselling-application-form.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { AppsettingService } from '../../Common/appsetting.service';
-import { CounsellingApplicationFormDataModel, CounsellingApplicationSearchModel } from '../../Models/CounsellingApplicationFormDataModel';
+import { Counselling_DropdownDataModel, Counselling_OptionFormDataModel, CounsellingApplicationFormDataModel, CounsellingApplicationSearchModel } from '../../Models/CounsellingApplicationFormDataModel';
 
 @Injectable({
   providedIn: 'root'
@@ -38,6 +38,30 @@ export class CounsellingApplicationFormService {
   public async GetApplicationDataByID_Counselling(request: CounsellingApplicationSearchModel) {
     var body = JSON.stringify(request);
     return await this.http.post(`${this.APIUrl}/GetApplicationDataByID_Counselling`, body, this.headersOptions)
+      .pipe(
+        catchError(this.handleErrorObservable)
+      ).toPromise();
+  }
+
+  public async Counselling_GetDropdownByAction(request: Counselling_DropdownDataModel) {
+    var body = JSON.stringify(request);
+    return await this.http.post(`${this.APIUrl}/Counselling_GetDropdownByAction`, body, this.headersOptions)
+      .pipe(
+        catchError(this.handleErrorObservable)
+      ).toPromise();
+  }
+
+  public async Counselling_SaveOption(request: Counselling_OptionFormDataModel) {
+    var body = JSON.stringify(request);
+    return await this.http.post(`${this.APIUrl}/Counselling_SaveOption`, body, this.headersOptions)
+      .pipe(
+        catchError(this.handleErrorObservable)
+      ).toPromise();
+  }
+
+  public async Counselling_GetOptionDetailsByID(request: Counselling_OptionFormDataModel) {
+    var body = JSON.stringify(request);
+    return await this.http.post(`${this.APIUrl}/Counselling_GetOptionDetailsByID`, body, this.headersOptions)
       .pipe(
         catchError(this.handleErrorObservable)
       ).toPromise();
