f24683ba1ebcde2e7049eb60c5ab5578d0157374
Jafar Alam <jafar.alam@corpnet.co.in>
2025-10-06 17:58:45 +0530

    counselling import candidate list changes

--- Kaushal_Darpan_Application/src/app/Views/Counselling-Master/counselling-import-candidate-list/counselling-import-candidate-list.component.ts
+++ Kaushal_Darpan_Application/src/app/Views/Counselling-Master/counselling-import-candidate-list/counselling-import-candidate-list.component.ts
@@ -96,7 +96,8 @@ export class CounsellingImportCandidateListComponent implements OnInit {
           this.ImportExcelFile(file);
         }
         this.selectedFile = null;
-    
+         // Reset file input so selecting the same file again triggers change
+        event.target.value = null;
       }
     ImportExcelFile(file: File): void {
         let mesg = '';
@@ -127,17 +128,37 @@ export class CounsellingImportCandidateListComponent implements OnInit {
             this.ImportExcelList = [];
              this.GetCandidateList(1);
           }
+          else{
+            this.toastr.error(data.ErrorMessage);
+          }
         });
     }
 
      async GetImportExcelDataPopup(content: any) {
 
     /*    this.IsShowViewStudent = true;*/
-    this.modalService.open(content, { size: 'xl', ariaLabelledBy: 'modal-basic-title', backdrop: 'static' }).result.then((result) => {
+    // this.modalService.open(content, { size: 'xl', ariaLabelledBy: 'modal-basic-title', backdrop: 'static' }).result.then((result) => {
+    //   this.closeResult = `Closed with: ${result}`;
+    // }, (reason) => {
+    //   this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    // });
+
+     this.modalRef1 = this.modalService.open(content, {
+    size: 'xl',
+    ariaLabelledBy: 'modal-basic-title',
+    backdrop: 'static'
+  });
+
+  this.modalRef1.result.then(
+    (result) => {
       this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
+      this.modalRef1 = null; // important
+    },
+    (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+      this.modalRef1 = null; // important
+    }
+  );
   }
 
 
@@ -210,25 +231,25 @@ export class CounsellingImportCandidateListComponent implements OnInit {
    get formEditData(){return this.EditDataFormGroup.controls;}
 
 
-    async GetTradeDDL() {
-    try {
-      this.loaderService.requestStarted();
-      await this.commonMasterService.ItiTrade(this.sSOLoginDataModel.DepartmentID, this.sSOLoginDataModel.Eng_NonEng, this.sSOLoginDataModel.EndTermID, this.sSOLoginDataModel.InstituteID)
-        .then((data: any) => {
-          console.log(data)
-          data = JSON.parse(JSON.stringify(data));
-          this.TradeDDLList = data['Data'];  
-        }, error => console.error(error));
-    }
-    catch (Ex) {
-      console.log(Ex);
-    }
-    finally {
-      setTimeout(() => {
-        this.loaderService.requestEnded();
-      }, 200);
-    }
-  }
+  //   async GetTradeDDL() {
+  //   try {
+  //     this.loaderService.requestStarted();
+  //     await this.commonMasterService.ItiTrade(this.sSOLoginDataModel.DepartmentID, this.sSOLoginDataModel.Eng_NonEng, this.sSOLoginDataModel.EndTermID, this.sSOLoginDataModel.InstituteID)
+  //       .then((data: any) => {
+  //         console.log(data)
+  //         data = JSON.parse(JSON.stringify(data));
+  //         this.TradeDDLList = data['Data'];  
+  //       }, error => console.error(error));
+  //   }
+  //   catch (Ex) {
+  //     console.log(Ex);
+  //   }
+  //   finally {
+  //     setTimeout(() => {
+  //       this.loaderService.requestEnded();
+  //     }, 200);
+  //   }
+  // }
 
   async GetCandidateList(i:any) {
     debugger
@@ -282,9 +303,9 @@ export class CounsellingImportCandidateListComponent implements OnInit {
   }
 
 
-  async Back() {
-    this.routers.navigate(['/CounsellingAllotmentList'])
-  }
+  // async Back() {
+  //   this.routers.navigate(['/CounsellingAllotmentList'])
+  // }
 
 
   //   checkboxthView_checkboxchange(isChecked: boolean) {
@@ -295,17 +316,19 @@ export class CounsellingImportCandidateListComponent implements OnInit {
   // }
 
   // get all data
-  async ClearSearchData() {
-    debugger
-    // this.searchRequest.Name = '';
-    // this.searchRequest.Enrollment = '';
-    // this.searchRequest.Category='';
-    // this.searchRequest.Status = '';
-    this.searchRequest.PageNumber = this.pageNo;
-    this.searchRequest.PageSize = this.pageSize;
-    await this.GetCandidateList(1);
+  
+  
+  // async ClearSearchData() {
+  //   debugger
+  //   this.searchRequest.Name = '';
+  //   this.searchRequest.Enrollment = '';
+  //   this.searchRequest.Category='';
+  //   this.searchRequest.Status = '';
+  //   this.searchRequest.PageNumber = this.pageNo;
+  //   this.searchRequest.PageSize = this.pageSize;
+  //   await this.GetCandidateList(1);
    
-  }
+  // }
 
 
 
@@ -356,8 +379,15 @@ export class CounsellingImportCandidateListComponent implements OnInit {
   
 
     CloseModalPopupimport() {
-    this.modalService.dismissAll();
+    // this.modalService.dismissAll();
     //this.ImportExcelList = [];
+
+      if (this.modalRef1) {
+        this.modalRef1.close();  // use close() or dismiss(), not dismissAll()
+        this.modalRef1 = null;
+      }
+      this.ImportExcelList = []; 
+      this.selectedFile = null;
   }
     private getDismissReason(reason: any): string {
       if (reason === ModalDismissReasons.ESC) {
@@ -382,7 +412,7 @@ export class CounsellingImportCandidateListComponent implements OnInit {
       });
     }
 
-      async GetCategoryMatserDDL() {
+  async GetCategoryMatserDDL() {
     try {
       this.AddCollegeWiseScholarshipModel.InstituteID = this.sSOLoginDataModel.DepartmentID
 
@@ -411,33 +441,35 @@ export class CounsellingImportCandidateListComponent implements OnInit {
     });
   }
 
-  async UnlockApplication_Counselling(item: any) {
-    this.Swal2.Confirmation(`Are you sure you want to Unlock Application for Candidate!`,
-      async (result: any) => {
-        //confirmed
-        if (result.isConfirmed) {
-          try {
-            this.unlockRequest.CandidateId = item.CandidateID;
-            await this.counsellingApplicationFormService.UnlockApplication_Counselling(this.unlockRequest)
-              .then(async (data: any) => {
-                data = JSON.parse(JSON.stringify(data));
-                if(data.State === EnumStatus.Success){
-                  this.toastr.success(data.Message);
-                  await this.GetCandidateList(1);
-                } else if(data.State === EnumStatus.Warning){
-                  this.toastr.warning(data.Message);
-                } else {
-                  this.toastr.error(data.ErrorMessage);
-                }
-            })
-          } catch (error) {
-            console.error(error);
-          }
-        }
-      });    
-  }
+  // async UnlockApplication_Counselling(item: any) {
+  //   this.Swal2.Confirmation(`Are you sure you want to Unlock Application for Candidate!`,
+  //     async (result: any) => {
+  //       //confirmed
+  //       if (result.isConfirmed) {
+  //         try {
+  //           this.unlockRequest.CandidateId = item.CandidateID;
+  //           await this.counsellingApplicationFormService.UnlockApplication_Counselling(this.unlockRequest)
+  //             .then(async (data: any) => {
+  //               data = JSON.parse(JSON.stringify(data));
+  //               if(data.State === EnumStatus.Success){
+  //                 this.toastr.success(data.Message);
+  //                 await this.GetCandidateList(1);
+  //               } else if(data.State === EnumStatus.Warning){
+  //                 this.toastr.warning(data.Message);
+  //               } else {
+  //                 this.toastr.error(data.ErrorMessage);
+  //               }
+  //           })
+  //         } catch (error) {
+  //           console.error(error);
+  //         }
+  //       }
+  //     });    
+  // }
 
   //table feature
+  
+  
   calculateInTableTotalPage() {
     this.totalInTablePage = Math.ceil(this.totalInTableRecord / parseInt(this.pageInTableSize));
   }
@@ -519,20 +551,20 @@ export class CounsellingImportCandidateListComponent implements OnInit {
     return this.sortInTableDirection == 'asc' ? '&uarr;' : '&darr;';
   }
   //checked all (replace org. list here)
-  selectInTableAllCheckbox() {
-    this.StudentList.forEach((x: any) => {
-      x.Selected = this.AllInTableSelect;
-    });
-  }
+  // selectInTableAllCheckbox() {
+  //   this.StudentList.forEach((x: any) => {
+  //     x.Selected = this.AllInTableSelect;
+  //   });
+  // }
   //checked single (replace org. list here)
-  selectInTableSingleCheckbox(isSelected: boolean, item: any) {
-    const data = this.StudentList.filter((x: any) => x.StudentID == item.StudentID);
-    data.forEach((x: any) => {
-      x.Selected = isSelected;
-    });
-    //select all(toggle)
-    this.AllInTableSelect = this.StudentList.every((r: any) => r.Selected);
-  }
+  // selectInTableSingleCheckbox(isSelected: boolean, item: any) {
+  //   const data = this.StudentList.filter((x: any) => x.StudentID == item.StudentID);
+  //   data.forEach((x: any) => {
+  //     x.Selected = isSelected;
+  //   });
+  //   //select all(toggle)
+  //   this.AllInTableSelect = this.StudentList.every((r: any) => r.Selected);
+  // }
   // end table feature
 
 
