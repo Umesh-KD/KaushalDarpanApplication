100c8bb0ce19ea833de07aaa0f4941a11ace06e1
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2025-10-31 10:59:40 +0530

    ncvt student admission list search u[pdated

---
+++ Kaushal_Darpan_Application/src/app/Views/ITI/ITI-Apprenticeship/nodal-college-approved-contract/nodal-college-approved-contract.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nodal-college-approved-contract',
+  standalone: false,
+  templateUrl: './nodal-college-approved-contract.component.html',
+  styleUrl: './nodal-college-approved-contract.component.css'
+})
+export class NodalCollegeApprovedContractComponent {
+
+}
