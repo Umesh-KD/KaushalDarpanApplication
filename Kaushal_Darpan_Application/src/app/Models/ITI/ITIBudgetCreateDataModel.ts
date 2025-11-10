cc5cf7efffb12c8d3506a1290649b17fde929a70
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2025-11-10 11:30:42 +0530

    ITI Budget changes

--- Kaushal_Darpan_Application/src/app/Models/ITI/ITIBudgetCreateDataModel.ts
+++ Kaushal_Darpan_Application/src/app/Models/ITI/ITIBudgetCreateDataModel.ts
@@ -1,3 +1,12 @@
-export class ITIBudgetDropdownDataModel{
+export class ITIBudgetDropdownDataModel {
     public Action?: string = '' 
+}
+
+export class ITIBudgetCreateDataModel {
+    public BudgetTypeID?: number = 0
+    public BudgetTypeName?: string = ''
+    public AcademicYearID?: number = 0
+    public BudgetForID?: number = 0
+    public BudgetType_Cumulative_HeadWise?: number = 0
+    public CumulativeAmount?: string = ''
 }
