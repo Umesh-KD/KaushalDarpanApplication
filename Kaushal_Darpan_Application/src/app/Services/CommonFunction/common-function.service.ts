791150be20766fe7380dad4a278319d6fa356be3
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2025-12-18 18:36:28 +0530

    BTER && ITI INVENTORY

--- Kaushal_Darpan_Application/src/app/Services/CommonFunction/common-function.service.ts
+++ Kaushal_Darpan_Application/src/app/Services/CommonFunction/common-function.service.ts
@@ -1076,7 +1076,7 @@ export class CommonFunctionService {
 
   public async GetITITradeList() {
 
-    return await this.http.get(this.APIUrl + '/TradeListGetAllData/', this.headersOptions)
+    return await this.http.post(this.APIUrl + '/TradeListGetAllData/' , this.headersOptions)
       .pipe(
         catchError(this.handleErrorObservable)
       ).toPromise();
