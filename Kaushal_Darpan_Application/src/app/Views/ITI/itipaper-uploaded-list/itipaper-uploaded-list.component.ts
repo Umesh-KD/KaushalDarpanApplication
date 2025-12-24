b103e211f9692cb556093e8e498fa030d8e3f165
vivek.kushwaha <vivek.kushwaha@RPHO05>
2025-12-24 13:13:12 +0530

    #vivek

--- Kaushal_Darpan_Application/src/app/Views/ITI/itipaper-uploaded-list/itipaper-uploaded-list.component.ts
+++ Kaushal_Darpan_Application/src/app/Views/ITI/itipaper-uploaded-list/itipaper-uploaded-list.component.ts
@@ -262,48 +262,113 @@ export class ITIPaperUploadedListComponent implements OnInit, AfterViewInit {
     this.modalService.dismissAll();
   }
 
+  //exportToExcelCenterDetail() {
+  //  const unwantedColumns = ['ActiveStatus', 'DeleteStatus', 'CreatedBy', 'ModifyBy', 'ModifyDate', 'IPAddress', 'CenterID', 'DownloadDate'];
+  //  const filteredData = this.CenterDtlsList.map(
+  //    (item: { [key: string]: any }, index: number) => {
+  //      const filteredItem: any = {
+  //        SNo: index + 1
+  //      };
+  //      Object.keys(item).forEach(key => {
+  //        if (!unwantedColumns.includes(key)) {
+  //          filteredItem[key] = item[key];
+  //        }
+  //      });
+  //      return filteredItem;
+  //    }
+  //  );
+  //  const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(filteredData);
+  //  const colWidths = Object.keys(filteredData[0]).map(key => {
+  //    let maxLength = key.length;
+  //    filteredData.forEach(function(row: { [x: string]: any; }) {
+  //      const cellValue = row[key];
+
+  //      let value = row[key];
+  //      if (key === 'IsDownload') {
+  //        value = value ? 'Yes' : 'No';
+  //      }
+  //            if (cellValue !== null && cellValue !== undefined) {
+  //                maxLength = Math.max(maxLength, cellValue.toString().length);
+  //            }
+  //        });
+  //    return { wch: maxLength + 2 };
+  //  });
+  //  ws['!cols'] = colWidths;
+  //  const range = XLSX.utils.decode_range(ws['!ref']!);
+  //  for (let C = range.s.c; C <= range.e.c; ++C) {
+  //    const cellAddress = XLSX.utils.encode_cell({ r: 0, c: C });
+  //    if (!ws[cellAddress]) continue;
+  //    ws[cellAddress].s = {
+  //      font: { bold: true }
+  //    };
+  //  }
+  //  const wb: XLSX.WorkBook = XLSX.utils.book_new();
+  //  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+  //  const today = new Date();
+  //  const dateStr = today.toLocaleDateString('en-GB').split('/').join('-');
+  //  const fileName = `Center_Paper_Donwload_Report_${dateStr}.xlsx`;
+  //  XLSX.writeFile(wb, fileName);
+  //}
+
   exportToExcelCenterDetail() {
-    const unwantedColumns = ['ActiveStatus', 'DeleteStatus', 'CreatedBy', 'ModifyBy', 'ModifyDate', 'IPAddress', 'CenterID', 'IsDownload','DownloadDate'];
+    const unwantedColumns = [
+      'ActiveStatus', 'DeleteStatus', 'CreatedBy', 'ModifyBy',
+      'ModifyDate', 'IPAddress', 'CenterID', 'DownloadDate'
+    ];
+
     const filteredData = this.CenterDtlsList.map(
       (item: { [key: string]: any }, index: number) => {
         const filteredItem: any = {
           SNo: index + 1
         };
+
         Object.keys(item).forEach(key => {
           if (!unwantedColumns.includes(key)) {
-            filteredItem[key] = item[key];
+            if (key === 'IsDownload') {
+              filteredItem[key] = item[key] ? 'Yes' : 'No';
+            } else {
+              filteredItem[key] = item[key];
+            }
           }
         });
+
         return filteredItem;
       }
     );
+
     const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(filteredData);
+
     const colWidths = Object.keys(filteredData[0]).map(key => {
       let maxLength = key.length;
-      filteredData.forEach(function(row: { [x: string]: any; }) {
-              const cellValue = row[key];
-              if (cellValue !== null && cellValue !== undefined) {
-                  maxLength = Math.max(maxLength, cellValue.toString().length);
-              }
-          });
+
+      filteredData.forEach((row: { [x: string]: any; }) => {
+        const cellValue = row[key];
+        if (cellValue !== null && cellValue !== undefined) {
+          maxLength = Math.max(maxLength, cellValue.toString().length);
+        }
+      });
+
       return { wch: maxLength + 2 };
     });
+
     ws['!cols'] = colWidths;
+
     const range = XLSX.utils.decode_range(ws['!ref']!);
     for (let C = range.s.c; C <= range.e.c; ++C) {
       const cellAddress = XLSX.utils.encode_cell({ r: 0, c: C });
-      if (!ws[cellAddress]) continue;
-      ws[cellAddress].s = {
-        font: { bold: true }
-      };
+      if (ws[cellAddress]) {
+        ws[cellAddress].s = { font: { bold: true } };
+      }
     }
+
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
     const today = new Date();
     const dateStr = today.toLocaleDateString('en-GB').split('/').join('-');
-    const fileName = `Center_Detail_List_${dateStr}.xlsx`;
-    XLSX.writeFile(wb, fileName);
+    XLSX.writeFile(wb, `Center_Paper_Donwload_Report_${dateStr}.xlsx`);
   }
+
 
 
 
