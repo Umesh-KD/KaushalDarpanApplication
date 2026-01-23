d97b5db8a57fd543c52ae8165fcf06cf2be80dc2
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2026-01-23 12:02:44 +0530

    report 33

--- Kaushal_Darpan_Application/src/app/Views/Reports/rpt-33/rpt-33.component.ts
+++ Kaushal_Darpan_Application/src/app/Views/Reports/rpt-33/rpt-33.component.ts
@@ -20,11 +20,13 @@ export class Rpt33Component {
   public ExamShiftDDL: any = []
   public sSOLoginDataModel = new SSOLoginDataModel()
   public request = new AttendanceRpt13BDataModel()
+  public downloadRequest = new AttendanceRpt13BDataModel()
   public StreamMasterDDL: any = []
   public SemesterMasterDDL: any = []
   public TableData: any = []
   // @ViewChild(OTPModuleComponent) childComponent!: OTPModuleComponent;
   CenterId: number= 0
+  examDate_pdf: any
 
   constructor(
     private commonMasterService: CommonFunctionService,
@@ -85,24 +87,29 @@ export class Rpt33Component {
   
 
   async onDownload(row: any) {
-    debugger
+     
     const formattedDate = this.formatDateToISO(row.ExamDate);
     try {
-      this.request.EndTermID = this.sSOLoginDataModel.EndTermID
-      this.request.Eng_NonEng = this.sSOLoginDataModel.Eng_NonEng
-      this.request.DepartmentID = this.sSOLoginDataModel.DepartmentID
-      this.request.SemesterID = row.semesterid;
-      this.request.SubjectID = row.SubjectId;
-      this.request.SubjectCode = row.PaperCode;
-      this.request.ShiftID = row.ExamShift;
-      this.request.ExamDate = formattedDate;
-      this.request.BranchCode=row.BranchCode;
+
+      let request: any = {}
+
+      this.downloadRequest.EndTermID = this.sSOLoginDataModel.EndTermID
+      this.downloadRequest.Eng_NonEng = this.sSOLoginDataModel.Eng_NonEng
+      this.downloadRequest.DepartmentID = this.sSOLoginDataModel.DepartmentID
+      this.downloadRequest.SemesterID = row.semesterid;
+      this.downloadRequest.SubjectID = row.SubjectId;
+      this.downloadRequest.SubjectCode = row.PaperCode;
+      this.downloadRequest.ShiftID = row.ExamShift;
+      this.downloadRequest.ExamDate = formattedDate;
+      this.downloadRequest.BranchCode=row.BranchCode;
+      this.downloadRequest.InstituteID = this.sSOLoginDataModel.InstituteID;
+      this.downloadRequest.UserID = this.sSOLoginDataModel.UserID
       
       if((this.sSOLoginDataModel.RoleID === EnumRole.Admin || this.sSOLoginDataModel.RoleID === EnumRole.AdminNon) && this.CenterId > 0){
-        this.request.InstituteID = this.CenterId;
+        request.InstituteID = this.CenterId;
       }
 
-      await this.reportService.Report33(this.request).then((data: any) => {
+      await this.reportService.Report33(this.downloadRequest).then((data: any) => {
         data = JSON.parse(JSON.stringify(data));
         if (data.State === EnumStatus.Success) {
           this.DownloadFile(data.Data)
@@ -154,7 +161,7 @@ export class Rpt33Component {
   }
 
   getSemesterCode(semesterId: number): string {
-    debugger
+     
     const semester = this.SemesterMasterDDL.find(
       (s: any) => s.SemesterID === Number(semesterId)
     );
@@ -164,7 +171,7 @@ export class Rpt33Component {
     // return semester ? semester.SemesterName.match(/\d/)?.[0] ?? '' : '';
   }
   getBranchCode(streamId: number): string {
-    debugger
+     
     const stream = this.StreamMasterDDL.find(
       (s:any) => s.StreamID === Number(streamId)
     );
@@ -182,21 +189,22 @@ export class Rpt33Component {
   generateFileName(extension: string): string {
     // const timestamp = new Date().toISOString().replace(/[:.-]/g, '_'); // Replace invalid characters
     // return `file_${timestamp}.${extension}`;
-    debugger
-    if (!this.request.ExamDate) {
+     
+    this.examDate_pdf 
+    if (!this.downloadRequest.ExamDate ) {
       this.toastr.error('Exam Date not selected');
       return '';
     }
     
-    const datePart = this.request.ExamDate.split('T')[0]; // "2025-12-16
+    const datePart = this.downloadRequest.ExamDate.split('T')[0]; // "2025-12-16
 
     const [yyyy, mm, dd] = datePart.split('-');
     const formattedDate = `${dd}${mm}${yyyy}`; // 16122025
          
     const instituteCode = this.getInstituteCode(this.sSOLoginDataModel.InstituteName);  
-    const semestercode = this.getSemesterCode(this.request.SemesterID); 
-    const branchCode = this.request.BranchCode;    
-    const papercode=this.request.SubjectCode;
+    const semestercode = this.getSemesterCode(this.downloadRequest.SemesterID); 
+    const branchCode = this.downloadRequest.BranchCode;    
+    const papercode=this.downloadRequest.SubjectCode;
   
     return `33_${formattedDate}_${instituteCode}_${semestercode}_${branchCode}_${papercode}.${extension}`;
 
