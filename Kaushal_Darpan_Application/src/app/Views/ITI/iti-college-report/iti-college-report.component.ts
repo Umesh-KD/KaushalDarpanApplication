774571c7f547080dd2c9cc852042c169ade10288
Shubh Saini <shubh.saini@corpnet.co.in>
2026-02-10 12:57:04 +0530

    commit

--- Kaushal_Darpan_Application/src/app/Views/ITI/iti-college-report/iti-college-report.component.ts
+++ Kaushal_Darpan_Application/src/app/Views/ITI/iti-college-report/iti-college-report.component.ts
@@ -746,9 +746,9 @@ export class ItiCollegeReportComponent {
     try {
       debugger
       this.isSubmitted = true;
-      if (this.ReportForm.invalid) {
-        return
-      }
+      //if (this.ReportForm.invalid) {
+      //  return
+      //}
 
       //if (this.request.IsNewCollege == 1) {
       //  if (this.NewReportFormGroup.invalid) {
@@ -1100,7 +1100,7 @@ export class ItiCollegeReportComponent {
 
     }
 
-    if (this.request.UrbanRural == 0) {
+    if (this.request.UrbanRural == 0 || this.sSOLoginDataModel.RoleID == EnumRole.ITIPlanningAdmin) {
       this.ReportForm.controls['VillageID'].clearValidators();
       this.ReportForm.controls['GramPanchayatSamiti'].clearValidators();
       this.ReportForm.controls['PanchayatId'].clearValidators();
