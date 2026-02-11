296b5634e32ecbbdd12a8ef7dba76bd189acc8d5
vivek.kushwaha <vivek.kushwaha@RPHO05>
2026-02-11 10:40:30 +0530

    #vivek

--- Kaushal_Darpan_Application/src/app/Views/ITI/reports/iti-All-Result/iti-All-Result.component.ts
+++ Kaushal_Darpan_Application/src/app/Views/ITI/reports/iti-All-Result/iti-All-Result.component.ts
@@ -21,6 +21,7 @@ import { OTPModalComponent } from '../../../otpmodal/otpmodal.component';
 import { HttpClient } from '@angular/common/http';
 import { CenterAllocationService } from '../../../../Services/Center_Allocation/center-allocation.service';
 import { ITICenterAllocationService } from '../../../../Services/ITICenterAllocation/ItiCenterAllocation.service';
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-iti-All-Result',
   standalone: false,
@@ -29,338 +30,41 @@ import { ITICenterAllocationService } from '../../../../Services/ITICenterAlloca
   styleUrl: './iti-All-Result.component.css'
 })
 export class itiAllResultComponent {
-  public State: number = -1;
-  public Message: any = [];
-  showDownloadOptions = false;
-  public tablerequest = new ReportBasedModel();
-  public ErrorMessage: any = [];
-  public isLoading: boolean = false;
-  public isSubmitted: boolean = false;
-  public Status: number = 0
-  public PublishFileName:string=''
-  public ITITimeTableList: any = [];
-  public UserID: number = 0;
-  searchText: string = '';
-  public TimeTablePdf: string = ''
-  public Dis_TimeTablePdf: string = ''
-  public IsYearly: number = 0
-  public isDisabledGrid: boolean = false;
-  public isDisabledDOJ: boolean = false;
-  isSubmittedItemDetails: boolean = false;
-  public isLoadingExport: boolean = false;
-  closeResult: string | undefined;
+
   modalReference: NgbModalRef | undefined;
-  searchbySemester: string = '';
-  searchbyExamShift: string = '';
-  searchbySubject: string = ''
-  public Table_SearchText: string = '';
-  public SearchTimeTableList: any = []
-  serchrequest = new InvigilatorSSOIDList();
-  public StreamMasterList: any = [];
-  public ExamShiftList: any = [];
-  public PaperList: any = [];
-  public SubjectList: any = [];
-  public SemesterList: any = [];
+  public Table_SearchText: string = "";
+  public allExcelDataList: any = []
+
   public searchRequest = new ITITimeTableSearchModel();
-  public request = new ITITimeTableDataModels();
-  public TimeTableTradeSubList: any = []
-  public InvigilatorDDL: InvigilatorSSOIDList[] = []
-  public InvigilatorFormGroup!: FormGroup;
-  public TimeTableID: number = 0
+
   sSOLoginDataModel = new SSOLoginDataModel();
-  public InvigilatorID: number = 0
-  public requestInv = new TimeTableInvigilatorModel()
-  public TimeTableInvigilatorData: any = [];
-  public requestInvigilatorSSOID = new DDL_InvigilatorSSOID_DataModel()
-  @ViewChild('otpModal') childComponent!: OTPModalComponent;
 
   constructor(
-    private streamService: StreamMasterService,
-    private commonMasterService: CommonFunctionService,
-    private Router: Router,
-    private ITITimeTableService: ITITimeTableService,
-    private toastr: ToastrService,
+  
     private loaderService: LoaderService,
-    private formBuilder: FormBuilder,
-    private modalService: NgbModal,
-    private Swal2: SweetAlert2,
     private reportService: ReportService,
     private appsettingConfig: AppsettingService,
-    private http: HttpClient,
-    private centerAllocationService: ITICenterAllocationService
+    private http: HttpClient
+    
   ) { }
 
   async ngOnInit() {
-    this.InvigilatorFormGroup = this.formBuilder.group({
-      InvigilatorID: ['', [DropdownValidators]],
-    })
     this.sSOLoginDataModel = await JSON.parse(String(localStorage.getItem('SSOLoginUser')));
-    console.log("sSOLoginDataModel", this.sSOLoginDataModel)
-    this.request.UserID = this.sSOLoginDataModel.UserID;
-    this.UserID = this.sSOLoginDataModel.UserID;
-    await this.GetITITimeTableList();
-    await this.GetStreamMasterList()
-    await this.GetExamShift()
-    await this.GetSubjectList()
-    await this.GetSemesterList()
-    await this.getMasterData()
+    await this.GetITIAllDataExcelReport()
   }
 
   
 
-  get _InvigilatorFormGroup() { return this.InvigilatorFormGroup.controls; }
 
-  async ViewandUpdate(content: any, id: number) {
-    this.TimeTableID = id
-    await this.GetTimeTableByID(id)
-    if (this.sSOLoginDataModel.RoleID == 7) {
-      await this.GetInvigilatorByID(id)
-    }
-    this.modalReference = this.modalService.open(content, { backdrop: 'static', size: 'xl', keyboard: true, centered: true });
-  }
-
-  async GetStreamMasterList() {
-    try {
+  async GetITIAllDataExcelReport() {
+    try
+    {
+      
       this.loaderService.requestStarted();
-      await this.streamService.GetAllData()
-        .then((data: any) => {
-          data = JSON.parse(JSON.stringify(data));
-          this.StreamMasterList = data['Data'];
-        }, error => console.error(error));
-    }
-    catch (Ex) {
-      console.log(Ex);
-    }
-    finally {
-      setTimeout(() => {
-        this.loaderService.requestEnded();
-      }, 200);
-    }
-  }
-
-  async GetExamShift() {
-    try {
-      this.loaderService.requestStarted();
-      var DepartmentID = EnumDepartment.ITI
-      await this.commonMasterService.GetExamShift(DepartmentID)
-        .then((data: any) => {
-          data = JSON.parse(JSON.stringify(data));
-          this.ExamShiftList = data['Data'];
-        }, error => console.error(error));
-    }
-    catch (Ex) {
-      console.log(Ex);
-    }
-    finally {
-      setTimeout(() => {
-        this.loaderService.requestEnded();
-      }, 200);
-    }
-  }
-
-  async GetSemesterList() {
-    try {
-      this.loaderService.requestStarted();
-      await this.commonMasterService.ITI_SemesterMaster()
-        .then((data: any) => {
-          data = JSON.parse(JSON.stringify(data));
-          this.SemesterList = data['Data'];
-        }, error => console.error(error));
-    }
-    catch (Ex) {
-      console.log(Ex);
-    }
-    finally {
-      setTimeout(() => {
-        this.loaderService.requestEnded();
-      }, 200);
-    }
-
-  }
-
-  async GetSubjectList() {
-    try {
-      this.loaderService.requestStarted();
-      await this.commonMasterService.GetSubjectMaster(this.sSOLoginDataModel.DepartmentID)
-        .then((data: any) => {
-          data = JSON.parse(JSON.stringify(data));
-          this.SubjectList = data['Data'];
-        }, error => console.error(error));
-    }
-    catch (Ex) {
-      console.log(Ex);
-    }
-    finally {
-      setTimeout(() => {
-        this.loaderService.requestEnded();
-      }, 200);
-    }
-  }
-
-  ResetControl() {
-    this.request = new ITITimeTableDataModels()
-    this.searchRequest = new ITITimeTableSearchModel()
-    this.GetITITimeTableList();
-  }
-  async GetITITimeTableList() {
-    debugger
-    this.searchRequest.DepartmentID = this.sSOLoginDataModel.DepartmentID;
-    this.searchRequest.EndTermID = this.sSOLoginDataModel.EndTermID
-    this.searchRequest.Eng_NonEng = this.sSOLoginDataModel.Eng_NonEng
-   
-
-    try {
-      if (this.sSOLoginDataModel.RoleID == 7) {
-        this.searchRequest.InstituteID = this.sSOLoginDataModel.InstituteID
-      }
-      this.loaderService.requestStarted();
-      await this.ITITimeTableService.GetAllData(this.searchRequest)
-        .then((data: any) => {
-          data = JSON.parse(JSON.stringify(data));
-          this.ITITimeTableList = data['Data'];
-          this.SearchTimeTableList = [...data['Data']];
-          this.Status=data['Data'][0]['Status']
-          this.PublishFileName = data['Data'][0]['FileName']
-          this.tablerequest.SemesterID = this.searchRequest.SemesterID
-          console.log("TimeTableList", this.ITITimeTableList)
-        }, error => console.error(error));
-    }
-    catch (Ex) {
-      console.log(Ex);
-    }
-    finally {
-      setTimeout(() => {
-        this.loaderService.requestEnded();
-      }, 200);
-    }
-  }
-
-
-  async btnDelete_OnClick(InstitudeID: number) {
-
-    this.Swal2.Confirmation("Are you sure you want to delete this ?",
-      async (result: any) => {
-        if (result.isConfirmed) {
-          try {
-            this.loaderService.requestStarted();
-
-            await this.ITITimeTableService.DeleteDataByID(InstitudeID, this.UserID, this.sSOLoginDataModel.DepartmentID)
-              .then(async (data: any) => {
-                data = JSON.parse(JSON.stringify(data));
-                console.log(data);
-
-                this.State = data['State'];
-                this.Message = data['Message'];
-                this.ErrorMessage = data['ErrorMessage'];
-
-                if (this.State = EnumStatus.Success) {
-                  this.toastr.success(this.Message)
-                  this.GetITITimeTableList()
-                }
-                else {
-                  this.toastr.error(this.ErrorMessage)
-                }
-
-              }, (error: any) => console.error(error)
-              );
-          }
-          catch (ex) {
-            console.log(ex);
-          }
-          finally {
-            setTimeout(() => {
-              this.loaderService.requestEnded();
-            }, 200);
-          }
-        }
-      });
-  }
-
-  CloseModalPopup() {
-    this.modalService.dismissAll();
-    this.requestInv = new TimeTableInvigilatorModel()
-  }
-
-  @ViewChild('content') content: ElementRef | any;
-
-  open(content: any, BookingId: string) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-
-  }
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
-
-  onEdit(id: number): void {
-    this.Router.navigate(['/updatetimetable', id]);
-    console.log(id)
-  }
-
-  async GetTimeTableByID(id: number) {
-    try {
-      this.loaderService.requestStarted();
-      await this.ITITimeTableService.GetTimeTableByID(id)
-        .then((data: any) => {
-          data = JSON.parse(JSON.stringify(data));
-          this.TimeTableTradeSubList = data.Data
-
-          console.log("TimeTableTradeSubList", this.TimeTableTradeSubList)
-
-        }, (error: any) => console.error(error));
-    }
-    catch (Ex) {
-      console.log(Ex);
-    }
-    finally {
-      setTimeout(() => {
-        this.loaderService.requestEnded();
-      }, 200);
-    }
-  }
-
-  async GetInvigilatorByID(id: number) {
-    try {
-      const InstituteID = this.sSOLoginDataModel.InstituteID
-      this.loaderService.requestStarted();
-      await this.ITITimeTableService.GetInvigilatorByID(id, InstituteID)
-        .then((data: any) => {
-          data = JSON.parse(JSON.stringify(data));
-          this.TimeTableInvigilatorData = data.Data
-          console.log("TimeTableInvigilatorData", this.TimeTableInvigilatorData)
-          if (this.TimeTableInvigilatorData != null) {
-            this.requestInv.CreatedBy = this.sSOLoginDataModel.UserID;
-            this.requestInv.InvigilatorID = this.TimeTableInvigilatorData.InvigilatorID
-          }
-        }, (error: any) => console.error(error));
-    }
-    catch (Ex) {
-      console.log(Ex);
-    }
-    finally {
-      setTimeout(() => {
-        this.loaderService.requestEnded();
-      }, 200);
-    }
-  }
-
-  async getMasterData() {
-    try {
-      this.requestInvigilatorSSOID.DepartmentID = this.sSOLoginDataModel.DepartmentID
-      this.loaderService.requestStarted();
-      await this.commonMasterService.Get_InvigilatorSSOID(this.requestInvigilatorSSOID).then((data: any) => {
+      await this.reportService.GetITIAllDataExcelReport(this.searchRequest).then((data: any) => {
         data = JSON.parse(JSON.stringify(data));
-        this.InvigilatorDDL = data.Data;
-        console.log("CourseMasterDDL", this.InvigilatorDDL);
+        this.allExcelDataList = data.Data;
+        console.log("CourseMasterDDL", this.allExcelDataList);
       })
 
     } catch (error) {
@@ -372,43 +76,39 @@ export class itiAllResultComponent {
     }
   }
 
-  async SaveInvigilator() {
-    this.isSubmitted = true;
-    if (this.InvigilatorFormGroup.invalid) {
-      this.toastr.error("Please select Invigilator")
-      return;
-    }
-    this.loaderService.requestStarted();
-    this.isLoading = true;
-    try {
-      this.requestInv.TimeTableID = this.TimeTableID
-      this.requestInv.DepartmentID = this.sSOLoginDataModel.DepartmentID;
-      this.requestInv.ModifyBy = this.sSOLoginDataModel.UserID;
-      this.requestInv.InstituteID = this.sSOLoginDataModel.InstituteID;
-      this.requestInv.SemesterID = this.TimeTableTradeSubList[0].SemesterID;
 
-      console.log("request at SaveInvigilator", this.requestInv)
-      await this.ITITimeTableService.SaveInvigilator(this.requestInv)
-        .then((data: any) => {
-          if (data.State == EnumStatus.Success) {
-            this.toastr.success(data.Message)
-            this.ResetControl();
-            this.CloseModalPopup();
-          } else {
-            this.toastr.error(data.ErrorMessage)
-          }
-        })
-    }
-    catch (ex) { console.log(ex) }
-    finally {
-      setTimeout(() => {
-        this.loaderService.requestEnded();
-        this.isLoading = false;
 
-      }, 200);
-    }
+   exportToExcel(): void {
+    const wantedColumns =
+      ['SrNo', 'Name'];
+
+    const exportData = this.allExcelDataList.map((row: any, index: number) => {
+      const filteredRow: any = {};
+      wantedColumns.forEach(col => {
+        filteredRow[col] = col === 'SrNo' ? index + 1 : row[col];
+      });
+      return filteredRow;
+    });
+
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(exportData);
+    const colWidths = wantedColumns.map(col => {
+      const maxLength = Math.max(
+        col.length,
+        ...exportData.map((row: { [x: string]: { toString: () => { (): any; new(): any; length: any; }; }; }) =>
+          row[col] ? row[col].toString().length : 0
+        )
+      );
+      return { wch: maxLength + 2 };
+    });
+    ws['!cols'] = colWidths;
+
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    const todayDate = new Date().toISOString().split('T')[0];
+
+    const fileName = `Campus_Details_Report_${todayDate}.xlsx`;
+    XLSX.writeFile(wb, fileName);
   }
-
 
   
 
