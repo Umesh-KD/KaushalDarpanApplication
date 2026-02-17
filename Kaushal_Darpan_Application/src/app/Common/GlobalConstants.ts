c0b4e3ff8e22ba156c270f16aecc552314c89dad
shubham.soni <shubham.soni@RPHO134>
2026-02-17 13:53:54 +0530

    commit office vacancy

--- Kaushal_Darpan_Application/src/app/Common/GlobalConstants.ts
+++ Kaushal_Darpan_Application/src/app/Common/GlobalConstants.ts
@@ -211,7 +211,9 @@ export enum EnumRole {
   EM_JDTE=239,
   EM_JD_BTER=48,
   EM_Secretary_BTER=243,
-  EM_NON_GAZETTED_STAFF=252
+  EM_NON_GAZETTED_STAFF=252,
+  EM_ADTE_GAZETTED_STAFF=240,
+  EM_ADTE_NON_GAZETTED_STAFF=241,
 }
 
 export enum enumExamStudentStatus {
