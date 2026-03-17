8c35ac7f8ea9ad1c14986161e3a6cf49b45820d3
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2026-03-17 11:54:01 +0530

    public result

--- Kaushal_Darpan_Application/src/app/Views/Home/search-result/search-result.component.ts
+++ Kaushal_Darpan_Application/src/app/Views/Home/search-result/search-result.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonFunctionService } from '../../../Services/CommonFunction/common-function.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { LoaderService } from '../../../Services/Loader/loader.service';
 import { ToastrService } from 'ngx-toastr';
-import { HomeService } from '../../../Services/Home/home.service';
-import { EnumDepartment, GlobalConstants } from '../../../Common/GlobalConstants';
+import { GlobalConstants } from '../../../Common/GlobalConstants';
 import { CampusDetailsWebSearchModel } from '../../../Models/CampusDetailsWebDataModel';
+import { StudentResultSearchModel } from '../../../Models/DownloadMarksheetDataModel';
 import { SSOLoginDataModel } from '../../../Models/SSOLoginDataModel';
-import { StreamMasterService } from '../../../Services/BranchesMaster/branches-master.service';
-import { AppsettingService } from '../../../Common/appsetting.service';
-import { HttpClient } from '@angular/common/http';
+import { CommonFunctionService } from '../../../Services/CommonFunction/common-function.service';
+import { LoaderService } from '../../../Services/Loader/loader.service';
+import { MarksheetDownloadService } from '../../../Services/MarksheetDownload/marksheet-download.service';
 
 
 @Component({
@@ -20,7 +16,7 @@ import { HttpClient } from '@angular/common/http';
     standalone: false
 })
 export class SearchResultComponent implements OnInit {
-
+  public resultSearchReq = new StudentResultSearchModel();
   public State: number = -1;
   public Message: any = [];
   public ErrorMessage: any = [];
@@ -36,6 +32,8 @@ export class SearchResultComponent implements OnInit {
   public searchBySemester: string = ''
   public searchByFinancialYearID: string = ''
 
+  public StudentResultData: any;
+
   minEndDate: string = '';
 
   searchModel = {
@@ -49,64 +47,29 @@ export class SearchResultComponent implements OnInit {
   InstituteID: number = 0;
   OriginalCampusPostList: any[] = []; // Store unfiltered data
   public BranchMasterList: any[] = [];
-  StreamID: number = 0;
-
- 
+  StreamID: number = 0; 
 
   constructor(
-    private streamService: StreamMasterService,
     private commonMasterService: CommonFunctionService,
-    private homeService: HomeService,
     private toastr: ToastrService,
     private loaderService: LoaderService,
-    private activatedRoute: ActivatedRoute,
-    private routers: Router,
-    private modalService: NgbModal,
-    private appsettingConfig: AppsettingService,
-    private http: HttpClient
+    private marksheetDownloadService: MarksheetDownloadService,
   ) {}
 
   async ngOnInit() {
     this.sSOLoginDataModel = await JSON.parse(String(localStorage.getItem('SSOLoginUser')));
-
     await this.GetSemesterList();
-    await this.GetFinancialYear();
-
-    await this.GetAllData();
-    //await this.GetStreamMasterList();
-    //this.loadDropdownData('FinancialYears');
-    //this.loadDropdownData('PlacementInstitute');
-   
+    await this.GetResultEndTermDDLList();   
   }
 
-
-  //loadDropdownData(MasterCode: string): void {
-  //  this.commonMasterService.GetCommonMasterData(MasterCode).then((data: any) => {
-  //    switch (MasterCode) {
-  //      case 'FinancialYears':
-  //        this.FinancialYear = data['Data'];
-  //        break;
-  //      case 'PlacementInstitute':
-  //        this.CollegeList = data['Data'];
-  //        break;
-  //      default:
-  //        break;
-  //    }
-  //  });
-  //}
-
   async GetSemesterList() {
-
     try {
       this.loaderService.requestStarted();
       await this.commonMasterService.SemesterMaster()
         .then((data: any) => {
           data = JSON.parse(JSON.stringify(data));
-          this.State = data['State'];
-          this.Message = data['Message'];
-          this.ErrorMessage = data['ErrorMessage'];
           this.SemesterList = data['Data'];
-
+          this.SemesterList = this.SemesterList.filter((x: any) => x.SemesterID !== 7 && x.SemesterID !== 8 && x.SemesterID !== 9);
         }, error => console.error(error));
     } catch (Ex) {
       console.log(Ex);
@@ -117,39 +80,12 @@ export class SearchResultComponent implements OnInit {
     }
   }
 
-
-  //async GetFinancialYear() {
-
-  //  try {
-  //    this.loaderService.requestStarted();
-  //    await this.commonMasterService.GetFinancialYear()
-  //      .then((data: any) => {
-  //        data = JSON.parse(JSON.stringify(data));
-  //        this.State = data['state'];
-  //        this.Message = data['Message'];
-  //        this.ErrorMessage = data['ErrorMessage'];
-  //        this.FinancialYear = data['Data'];
-
-  //      }, error => console.error(error));
-  //  } catch (Ex) {
-  //    console.log(Ex);
-  //  } finally {
-  //    setTimeout(() => {
-  //      this.loaderService.requestEnded();
-  //    }, 200);
-  //  }
-  //}
-
-  async GetFinancialYear() {
+  async GetResultEndTermDDLList() {
     this.loaderService.requestStarted();
 
     try {
-      const data: any = await this.commonMasterService.GetFinancialYear();
+      const data: any = await this.marksheetDownloadService.GetResultEndTermDDLList();
       const parsedData = JSON.parse(JSON.stringify(data)); // Not ideal, see note below
-
-      this.State = parsedData['state'];
-      this.Message = parsedData['Message'];
-      this.ErrorMessage = parsedData['ErrorMessage'];
       this.FinancialYear = parsedData['Data'];
 
     } catch (error) {
@@ -161,132 +97,17 @@ export class SearchResultComponent implements OnInit {
     }
   }
 
-
-
-  async GetAllData() {
-    debugger
+  async GetStudentResult_public() {
     try {
-      this.loaderService.requestStarted();
-      await this.homeService.GetAllPost(this.PostId, EnumDepartment.BTER, this.StreamID, this.CampusFromDate, this.CampusToDate, this.FinancialYearID, this.InstituteID)
-        .then((data: any) => {
-          
-          data = JSON.parse(JSON.stringify(data));
-          this.CampusPostList = data['Data'];
-          console.log('Campus Post List ==>',this.CampusPostList)
-        }, (error: any) => console.error(error)
-        );
-    }
-    catch (ex) {
-      console.log(ex);
-    }
-    finally {
-      setTimeout(() => {
-        this.loaderService.requestEnded();
-      }, 200);
+      debugger
+      await this.marksheetDownloadService.GetStudentResult_public(this.resultSearchReq).then(async (data: any) => {
+        data = JSON.parse(JSON.stringify(data));
+        this.StudentResultData = data['Data'];
+      })
+    } catch (error) {
+      console.error(error);
     }
   }
-
-  //async GetAllPlacementCompany() {
-  //  this.searchRequest.DepartmentID = EnumDepartment.BTER
-  //  try {
-  //    this.loaderService.requestStarted();
-  //    await this.homeService.GetAllPlacementCompany(this.searchRequest)
-  //      .then((data: any) => {
-  //        data = JSON.parse(JSON.stringify(data));
-  //        console.log(data);
-  //        this.PlacementCompanyList = data['Data'];
-  //      }, (error: any) => console.error(error)
-  //      );
-  //  }
-  //  catch (ex) {
-  //    console.log(ex);
-  //  }
-  //  finally {
-  //    setTimeout(() => {
-  //      this.loaderService.requestEnded();
-  //    }, 200);
-  //  }
-  //}
-
-  //async GetStreamMasterList() {
-  //  try {
-  //    this.loaderService.requestStarted();
-  //    await this.streamService.GetAllData()
-  //      .then((data: any) => {
-  //        data = JSON.parse(JSON.stringify(data));
-  //        console.log("data of BranchMasterList", data)
-  //        this.BranchMasterList = data['Data'];
-
-  //        console.log("Branch Master List ===>", this.BranchMasterList)
-  //      }, error => console.error(error));
-  //  }
-  //  catch (Ex) {
-  //    console.log(Ex);
-  //  }
-  //  finally {
-  //    setTimeout(() => {
-  //      this.loaderService.requestEnded();
-  //    }, 200);
-  //  }
-
-  //}
-
-
-  updateEndDateMin() {
-    this.minEndDate = this.searchModel.startDate;
-    if (this.searchModel.endDate && this.searchModel.endDate < this.searchModel.startDate) {
-      this.searchModel.endDate = '';
-    }
-  }
-  
-  
-
-  onSearch() {
-    const fromDate = this.CampusFromDate ? this.stripTime(new Date(this.CampusFromDate)) : null;
-    const toDate = this.CampusToDate ? this.stripTime(new Date(this.CampusToDate)) : null;
-    debugger;
-    this.CampusPostList = this.OriginalCampusPostList.filter(item => {
-      const itemDate = this.stripTime(new Date(item.PostDate));
-
-      // Date filter
-      if (fromDate && itemDate < fromDate) return false;
-      if (toDate && itemDate > toDate) return false;
-
-      // Branch filter
-      if (this.StreamID && this.StreamID !== 0 && item.StreamID !== this.StreamID) {
-        return false;
-      }
-
-      return true;
-    });
-  }
-
-
-  private stripTime(date: Date): Date {
-    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
-  }
-
-  //async DownloadPdf(FileName: string) {
-  //  debugger;
-  //  const fileUrl = this.appsettingConfig.StaticFileRootPathURL + "/" + FileName;; // Replace with your URL
-  //  // Fetch the file as a blob
-  //  try {
-  //    this.http.get(fileUrl, { responseType: 'blob' }).subscribe((blob: any) => {
-  //      const downloadLink = document.createElement('a');
-  //      const url = window.URL.createObjectURL(blob);
-  //      downloadLink.href = url;
-  //      downloadLink.download = "" + FileName + ".pdf"; // Set the desired file name
-  //      downloadLink.click();
-  //      // Clean up the object URL
-  //      window.URL.revokeObjectURL(url);
-  //    });
-  //  }
-  //  catch (error) {
-  //    console.error('Error downloading file:', error);
-  //    this.toastr.error('Failed to download file.', 'Error');
-  //  }
-
-  //}
 
 
 }
