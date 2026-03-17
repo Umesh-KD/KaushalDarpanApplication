78ef3076eb688edd9188f878ecf915feb70117a2
Shubh Saini <shubh.saini@corpnet.co.in>
2026-03-17 12:54:34 +0530

    commit

--- Kaushal_Darpan_Application/src/app/Views/ITI/Direct-Addmission-Form-Private-ITI/iti-direct-preview-private-form/iti-direct-preview-private-form.component.ts
+++ Kaushal_Darpan_Application/src/app/Views/ITI/Direct-Addmission-Form-Private-ITI/iti-direct-preview-private-form/iti-direct-preview-private-form.component.ts
@@ -173,8 +173,15 @@ export class ITIDirectPreviewPrivateFormComponent {
             })
             debugger
             this.request.QualificationViewDetails = data.Data.QualificationViewDetails
-            this.Techqualificalrequest = this.request.QualificationViewDetails.filter((e: any) => e.QualificationName == 'Technical')
-            this.request.QualificationViewDetails = this.request.QualificationViewDetails.filter((e: any) => e.QualificationName != 'Technical')
+            this.Techqualificalrequest =
+              this.request?.QualificationViewDetails?.filter(
+                (e: any) => e?.QualificationName === 'Technical'
+              ) ?? [];
+
+            this.request.QualificationViewDetails =
+              this.request?.QualificationViewDetails?.filter(
+                (e: any) => e?.QualificationName !== 'Technical'
+              ) ?? [];
             console.log("this.request", this.request)
             const dob = new Date(data['Data']['DOB']);
             const year = dob.getFullYear();
@@ -183,7 +190,7 @@ export class ITIDirectPreviewPrivateFormComponent {
             this.request.DOB = `${year}-${month}-${day}`;
 
             this.ShowHideButtons(this.request.IsfinalSubmit, this.request.IsFinalPay, this.request.DirectAdmissionType);
-
+            debugger
             if (this.request.PendingDataModel?.length > 0) { this.IsShowIncompleteData = true }
             else { this.IsShowIncompleteData = false }
           }
