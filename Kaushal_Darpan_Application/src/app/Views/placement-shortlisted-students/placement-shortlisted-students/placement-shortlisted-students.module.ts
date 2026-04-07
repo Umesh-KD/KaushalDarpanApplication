aa417b9742b380e9572d9660256e459c47be9b9f
shubham.soni <shubham.soni@RPHO134>
2026-04-07 18:45:24 +0530

    bter placement issues resolved

--- Kaushal_Darpan_Application/src/app/Views/placement-shortlisted-students/placement-shortlisted-students/placement-shortlisted-students.module.ts
+++ Kaushal_Darpan_Application/src/app/Views/placement-shortlisted-students/placement-shortlisted-students/placement-shortlisted-students.module.ts
@@ -6,6 +6,7 @@ import { PlacementShortlistedStudentsComponent } from './placement-shortlisted-s
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoaderModule } from '../../Shared/loader/loader.module';
 import { TableSearchFilterModule } from '../../../Pipes/table-search-filter.module';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 
 @NgModule({
@@ -15,7 +16,7 @@ import { TableSearchFilterModule } from '../../../Pipes/table-search-filter.modu
   imports: [
     CommonModule,
     PlacementShortlistedStudentsRoutingModule
-    , FormsModule, ReactiveFormsModule, CommonModule, LoaderModule, TableSearchFilterModule
+    , FormsModule, ReactiveFormsModule, CommonModule, LoaderModule, TableSearchFilterModule,NgSelectModule
   ]
 })
 export class PlacementShortlistedStudentsModule { }
