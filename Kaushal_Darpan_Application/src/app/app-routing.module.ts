d4eaffd4a701e24007b192d6e04e209aa8f7ba98
shubham.soni <shubham.soni@RPHO134>
2026-04-23 18:54:41 +0530

    Deepak sain

--- Kaushal_Darpan_Application/src/app/app-routing.module.ts
+++ Kaushal_Darpan_Application/src/app/app-routing.module.ts
@@ -1212,7 +1212,7 @@ const routes: Routes = [
       { path: 'transfer-request-accept', loadChildren: () => import('./Views/ITI/ITI-GOVT-EstablishManagement/UserRequest/transfer-request-accept/transfer-request-accept.module').then(m => m.TransferRequestAcceptModule), title: 'User Request List' },
       { path: 'PostPlanning', loadChildren: () => import('./Views/ITI/post-planning/post-planning.module').then(m => m.PostPlanningModule) },
       { path: 'AddPostPlanning', loadChildren: () => import('./Views/ITI/add-post-planning/add-post-planning.module').then(m => m.AddPostPlanningModule) },
-
+      { path: 'OfficeVacancyList', loadChildren: () => import('./Views/ITI/ITI-GOVT-EstablishManagement/office-vacancy-list/office-vacancy-list.module').then(m => m.OfficeVacancyListModule) },
       // ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,, ITI Establishment END ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 
       { path: 'PaperSetterProfessorDashboard', loadChildren: () => import('./Views/ITI/itipapper-setter/itipaper-setter-professor-dashboard/itipaper-setter-professor-dashboard.module').then(m => m.ITIPaperSetterProfessorDashboardModule) },
