3f3d83237cbc6e093c9c90526c180294f8a38c1c
Umesh Rajpoot <umesh.rajpoot@corpnet.co.in>
2026-04-28 18:51:02 +0530

    Code commit

--- Kaushal_Darpan_Application/src/app/app-routing.module.ts
+++ Kaushal_Darpan_Application/src/app/app-routing.module.ts
@@ -1817,6 +1817,7 @@ const routes: Routes = [
   { path: 'Views\ITI\InstructorSelectchoice', loadChildren: () => import('./Views/ITI/instructor-selectchoice/instructor-selectchoice.module').then(m => m.InstructorSelectchoiceModule) },
   { path: 'Views\ITI\NodalMapping', loadChildren: () => import('./Views/ITI/nodal-mapping/nodal-mapping.module').then(m => m.NodalMappingModule) },
   { path: 'Views\ScholarshipGetData', loadChildren: () => import('./Views/scholarship-get-data/scholarship-get-data.module').then(m => m.ScholarshipGetDataModule) },
+  { path: 'PostMaster', loadChildren: () => import('./Views/ITI/post-master/post-master.module').then(m => m.PostMasterModule) },
 
 
 
