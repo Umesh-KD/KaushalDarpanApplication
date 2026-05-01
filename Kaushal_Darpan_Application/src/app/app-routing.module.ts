d672fc0711d7be87abe42b0ccae581333c9dcda0
shubham.soni <shubham.soni@RPHO134>
2026-05-01 11:33:11 +0530

    Deepak sain

--- Kaushal_Darpan_Application/src/app/app-routing.module.ts
+++ Kaushal_Darpan_Application/src/app/app-routing.module.ts
@@ -1174,6 +1174,8 @@ const routes: Routes = [
       { path: 'em-add-training-details', loadChildren: () => import('./Views/BTER/BTER-GOVT-Establish-Management/em-add-training-details/em-add-training-details.module').then(m => m.EMAddTrainingDetailsModule), title: 'Training Details' },
       { path: 'Staff-Completed-Process-Training', loadChildren: () => import('./Views/BTER/BTER-GOVT-Establish-Management/em-training-details-history/em-training-details-history.module').then(m => m.emtrainingdetailshistoryModule), title: 'Training Details' },
       { path: 'Staff-New-Process-Training', loadChildren: () => import('./Views/BTER/BTER-GOVT-Establish-Management/EM-Staff-New-Process-Training/EM-Staff-New-Process-Training.module').then(m => m.EMStaffNewProcessTrainingModule), title: 'Training Details' },
+      { path: 'ShorTrainingDetailsList', loadChildren: () => import('./Views/BTER/BTER-GOVT-Establish-Management/EM_TrainingDetailsList/EM_TrainingDetailsList.module').then(m => m.EM_TrainingDetailsListModule), title: 'Training Details' },
+
       
       { path: 'Bter-AddUserRequest', loadChildren: () => import('./Views/BTER/BTER-GOVT-Establish-Management/bter-em-UserRequest/bter-em-request-add/bter-em-request-add.module').then(m => m.BtereEMRequestAddModule), title: 'Bter-AddUserRequest' },
       { path: 'Bter-AddUserRequest/:id', loadChildren: () => import('./Views/BTER/BTER-GOVT-Establish-Management/bter-em-UserRequest/bter-em-request-add/bter-em-request-add.module').then(m => m.BtereEMRequestAddModule), title: 'Bter-AddUserRequest' },
