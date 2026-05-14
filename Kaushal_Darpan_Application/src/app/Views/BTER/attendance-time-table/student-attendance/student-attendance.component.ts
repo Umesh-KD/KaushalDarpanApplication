669389ddaad7412b1f8a1e38191f29f5ce7b0f38
Umesh Rajpoot <umesh.rajpoot@corpnet.co.in>
2026-05-14 19:29:10 +0530

    branch-section commit

--- Kaushal_Darpan_Application/src/app/Views/BTER/attendance-time-table/student-attendance/student-attendance.component.ts
+++ Kaushal_Darpan_Application/src/app/Views/BTER/attendance-time-table/student-attendance/student-attendance.component.ts
@@ -102,11 +102,11 @@ export class StudentAttendanceComponent implements OnInit {
     this.TableForm = this.fb.group({
       SubjectID: ['', Validators.required],
       AttandanceTimeID: ['', Validators.required],
-      StreamID: ['', Validators.required],
-      // StreamID: [{ value: '', disabled: true }, Validators.required],
+      //StreamID: ['', Validators.required],
+       StreamID: [{ value: '', disabled: true }, Validators.required],
       SectionID: ['', Validators.required],
-      SemesterID: ['', Validators.required],
-      // SemesterID: [{ value: '', disabled: true }, Validators.required],
+     // SemesterID: ['', Validators.required],
+       SemesterID: [{ value: '', disabled: true }, Validators.required],
       AttendanceStartDate: [this.selectedRange?.start],
       AttendanceEndDate: [this.selectedRange?.end]
     });
@@ -282,7 +282,7 @@ export class StudentAttendanceComponent implements OnInit {
 
   async GetStaffLeaveAllData() {
     try {
-      //debugger
+      debugger
       const rawStart = this.TableForm.value.AttendanceStartDate;
       const rawEnd = this.TableForm.value.AttendanceEndDate;
 
@@ -455,13 +455,13 @@ export class StudentAttendanceComponent implements OnInit {
           : this.formatDate(rawEnd);
 
       let obj = {
-        SemesterID: this.TableForm.value.SemesterID,
+        SemesterID: this.semesterId,
         EndTermID: this.sSOLoginDataModel.EndTermID,
         InstituteID: this.sSOLoginDataModel.InstituteID,
         DepartmentID: this.sSOLoginDataModel.DepartmentID,
         CourseTypeID: this.sSOLoginDataModel.Eng_NonEng,
-        StreamID: this.TableForm.value.StreamID,
-        SectionID: this.TableForm.value.SectionID,
+        StreamID: this.streamId,
+        SectionID: this.sectionId,
         SubjectID: this.subjectId,
         AttendanceStartDate: formattedDateStart,
         AttendanceEndDate: formattedDateEnd,
@@ -768,7 +768,7 @@ export class StudentAttendanceComponent implements OnInit {
     // if (this.TableForm.value.StreamID != null && this.TableForm.value.SubjectID) {
     //   this.GetAttendanceTimeTable();
     // }
-    if (this.TableForm.value.StreamID != null && this.subjectId != null) {
+    if (this.streamId != null && this.subjectId != null) {
       await this.GetAttendanceTimeTable();
       await this.GetAttendanceMarkingStatus();
     }
@@ -901,8 +901,8 @@ export class StudentAttendanceComponent implements OnInit {
 
       const attendanceData = {
         EndTermID: this.sSOLoginDataModel.EndTermID,
-        SemesterID: this.TableForm.value.SemesterID,
-        StreamID: this.TableForm.value.StreamID,
+        SemesterID: this.semesterId,
+        StreamID: this.streamId,
         SectionID: this.sectionId,
         // SubjectID: this.TableForm.value.SubjectID,
         SubjectID: this.subjectId,
@@ -980,9 +980,13 @@ export class StudentAttendanceComponent implements OnInit {
   async ChangeSubjectDDL() {
 
     // debugger
-    const GetSemesterID = this.TableForm.get('SemesterID')?.value;
-    const GetstreamId = this.TableForm.get('StreamID')?.value;
-    const GetSubjectID = this.TableForm.get('SubjectID')?.value;
+    //const GetSemesterID = this.TableForm.get('SemesterID')?.value;
+    //const GetstreamId = this.TableForm.get('StreamID')?.value;
+    //const GetSubjectID = this.TableForm.get('SubjectID')?.value;
+
+    const GetSemesterID = this.semesterId;
+    const GetstreamId = this.streamId;
+    const GetSubjectID = this.subjectId;
 
     //debugger
     let obj = {
