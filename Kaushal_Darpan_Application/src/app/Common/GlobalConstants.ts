a7fe9c8ebcafbe1e31e23363eaf3f060e1c49d98
Raviraj.singh <Raviraj.singh@RPHO139>
2026-05-20 16:17:48 +0530

    #RAVI

--- Kaushal_Darpan_Application/src/app/Common/GlobalConstants.ts
+++ Kaushal_Darpan_Application/src/app/Common/GlobalConstants.ts
@@ -728,6 +728,11 @@ export enum EnumWS_DepartmentSub {
   DTE = 242,
   ITI_NCVT = 243,
   ITI_SCVT = 244,
+
+  ITI_Planing = 245,
+  ITI_Establishment = 246,
+  ITI_Purchase =247
+
 }
 
 export enum EnumITIDispatchDDlValue {
