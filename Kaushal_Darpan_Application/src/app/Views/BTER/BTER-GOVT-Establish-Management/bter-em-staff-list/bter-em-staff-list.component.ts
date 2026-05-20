caee5006280e3a0790bf4725b2c5f4a674a39bc2
Ramesh Kumawat <ramesh.kumawat@corpnet.co.in>
2026-05-20 14:30:14 +0530

    guest house dte bter ttc

--- Kaushal_Darpan_Application/src/app/Views/BTER/BTER-GOVT-Establish-Management/bter-em-staff-list/bter-em-staff-list.component.ts
+++ Kaushal_Darpan_Application/src/app/Views/BTER/BTER-GOVT-Establish-Management/bter-em-staff-list/bter-em-staff-list.component.ts
@@ -1178,6 +1178,7 @@ debugger
       this.loaderService.requestStarted();
       this.searchRequest1.GuestHouseIDs = this.sSOLoginDataModel.GuestHouseID;
       this.searchRequest1.isEstablishment = true
+      this.searchRequest1.RoleId = this.sSOLoginDataModel.RoleID
       await this.guestRoomManagmentService.GetGuestHouseNameList(this.searchRequest1)
         .then((data: any) => {
           data = JSON.parse(JSON.stringify(data));
