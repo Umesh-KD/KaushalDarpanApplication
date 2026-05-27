a78f15b4ac0e696254f2d8476ffd7f924266fae4
Raviraj.singh <Raviraj.singh@RPHO139>
2026-05-27 19:29:20 +0530

    #RAVi

--- Kaushal_Darpan_Application/src/app/Models/IndustryInstitutePartnershipMasterDataModel.ts
+++ Kaushal_Darpan_Application/src/app/Models/IndustryInstitutePartnershipMasterDataModel.ts
@@ -148,6 +148,9 @@ export class CompanyEventSearchModel extends RequestBaseModel {
   public CompanyID: number = 0
   public EventID: number = 0
   public StaffID: number = 0
+  public EventStatus: number = 0
   public InterestedStatus: number = 0
-  public Remarks:string=''
+  public Remarks: string = ''
+
+
 }
