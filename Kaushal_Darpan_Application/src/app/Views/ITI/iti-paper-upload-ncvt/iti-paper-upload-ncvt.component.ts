fd1e6055efe67a0fb52805748e4d0be69a014236
Niranjan Pandey <Niranjan.Pandey@devitpl.com>
2026-06-04 12:04:53 +0530

    ncvt paper upload component

---
+++ Kaushal_Darpan_Application/src/app/Views/ITI/iti-paper-upload-ncvt/iti-paper-upload-ncvt.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iti-paper-upload-ncvt',
+  standalone: false,
+  templateUrl: './iti-paper-upload-ncvt.component.html',
+  styleUrl: './iti-paper-upload-ncvt.component.css'
+})
+export class ItiPaperUploadNcvtComponent {
+
+}
