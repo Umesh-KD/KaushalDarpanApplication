4e68ba7b2de41bd55db5f906367dece2c5bf5902
vivek.kushwaha <vivek.kushwaha@RPHO05>
2026-06-30 16:24:29 +0530

    applyduplicate document fileroot path saved

--- Kaushal_Darpan_Application/src/app/Views/Student/payment-status/payment-status.component.ts
+++ Kaushal_Darpan_Application/src/app/Views/Student/payment-status/payment-status.component.ts
@@ -84,7 +84,7 @@ export class PaymentStatusComponent implements OnInit {
   }
 
   async FeeReceipt() {
-    
+    debugger
     if (this.DepartmentID == EnumDepartment.ITI) {
       this.GetITIStudentFeeReceipt()
     }
